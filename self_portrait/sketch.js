3ea19c050c9b31194529b2229af05a789c6a92f4
Jordan721 <42786923+Jordan721@users.noreply.github.com>
2018-09-06 14:47:21 -0400

    Making "My Portrait"
    Self Portrait 1
    9/6/2018

--- self_portrait/sketch.js
+++ self_portrait/sketch.js
@@ -1,25 +1,40 @@
+/*
+Self Portrait 1
+By Jordan Alexis 
+MMP210
+*/
 function setup(){
     createCanvas (640, 360);
 }
 
 function draw(){
-    background("darkgreen")
+    background("darkgreen");
     
-    //eyes
+     //face
+    rectMode(CENTER)
+    fill("brown");
+    ellipse (220, 20, 220, 420);
+    
+    //hair
+    fill("black")
+    rect(220, 20, 100, 120);
+    
+    //cheeks
     fill("blue");
-    ellipse(320, 180, 100);
-    ellipse(200, 180, 100);
-    
-    //face
-    fill("black");
-    rect(320, 20, 100, 100);
-    
-    //ears
+    ellipse(50, 100, 40);
+    ellipse(50, 200, 40);
+      
+    //eyes
     fill("orange")
-    ellipse(250, 180, 100);
-    ellipse(120, 180, 100);
+    ellipse(250, 70, 30);
+    ellipse(120, 70, 30);
+    
+    //nose
+    stroke("gray")
+    rect(75, 125, 20, 20);
     
     //mouth
-    stroke("gray")
-    line(152, 250, 250, 280)
+    stroke("red")
+    rect(75, 125, 70, 10, 10);
+    
 }
