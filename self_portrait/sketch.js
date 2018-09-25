8e3fb59fedd785e63590b01de173a0cb9b3b2715
Jordan721 <42786923+Jordan721@users.noreply.github.com>
2018-09-25 14:55:21 -0400

    Fixed collums
    9/25/2018

--- self_portrait/sketch.js
+++ self_portrait/sketch.js
@@ -3,32 +3,33 @@ Self Portrait 1
 By Jordan Alexis 
 MMP210
 */
-function setup(){
-    createCanvas (640, 360);
+function setup() {
+    createCanvas(640, 360);
 }
-     function draw(){
+
+function draw() {
     background(200);
-         
-     //face
+
+    //face
     rectMode(CENTER)
     fill("brown");
-    ellipse (220, 20, 220, 420);
-    
+    ellipse(220, 20, 220, 420);
+
     //stripe
     fill("black")
     rect(220, 20, 60, 340);
-    
-    
+
+
     //eyes
     fill("orange")
     ellipse(250, 90, 70);
     ellipse(120, 70, 60);
-    
+
     //nose
     stroke("gray")
     fill("gray")
     rect(175, 125, 50, 50);
-    
+
     //mouth
     stroke("red")
     fill("blue")
--- self_portrait_interactive/sketch.js
+++ self_portrait_interactive/sketch.js
@@ -3,57 +3,56 @@ Self Portrait 3 Interactive
 By Jordan Alexis 
 MMP210
 */
-function setup(){
-    createCanvas (640, 360);
+function setup() {
+    createCanvas(640, 360);
 }
 
-function draw(){
-    background ("gray");
+function draw() {
+    background("gray");
 
-var x= width/17;
-var y= height/20;
-var y = height/20;
-var s = 200; // size
-var o = 50; // offset
- var eyesize= 50;
-var faceColor = "brown"
-var puiplsize1= 20;
-var puiplsize2= 30;
-var facesize= 420;
+    var x = width / 17;
+    var y = height / 20;
+    var y = height / 20;
+    var s = 200; // size
+    var o = 50; // offset
+    var eyesize = 50;
+    var faceColor = "brown"
+    var puiplsize1 = 20;
+    var puiplsize2 = 30;
+    var facesize = 420;
 
-//face
+    //face
     rectMode(CENTER)
     fill(faceColor);
-    ellipse (220, 20, 220, facesize);
-    
+    ellipse(220, 20, 220, facesize);
+
     //stripe
     fill("black")
     rect(220, 20, 60, 340);
-    
-    
+
+
     //eyes
     fill("orange")
     ellipse(270, 90, eyesize);
     ellipse(120, 70, eyesize);
-         
+
     //nose
     stroke("gray")
     fill("gray")
     rect(220, 125, 50, 50);
-    
+
     //mouth
     stroke("red")
     strokeWeight(4);
-    fill("blue")
-         
-          //puipls
-         fill("teal")
-         ellipse(270, 90, puiplsize1);
-         ellipse(120, 70, puiplsize2);
-    
+
+
+    //puipls
+    fill("red")
+    ellipse(270, 90, puiplsize1);
+    ellipse(120, 70, puiplsize2);
+
     var mouthSize = map(mouseX, 0, width, 5, 200); // move mouth with mouse x
-	mouthSize = max(0, mouthSize); // prevents negative value error
-
-	rect(220, 125 + 80, s/2, mouthSize, 175); // mouth
+    mouthSize = max(0, mouthSize); // prevents negative value error
+    fill("blue")
+    rect(220, 125 + 80, s / 2, mouthSize, 175); // mouth
 }
-
