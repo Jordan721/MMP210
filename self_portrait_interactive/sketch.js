ff19669a19a68107376eea0811dd3b10b35f3ca2
Jordan721 <42786923+Jordan721@users.noreply.github.com>
2018-09-25 12:04:30 -0400

    Doing The Interactive
    9/25/2018

--- self_portrait_interactive/sketch.js
+++ self_portrait_interactive/sketch.js
@@ -7,15 +7,21 @@ function setup(){
     createCanvas (640, 360);
 }
 
-    var eyesize= 50;
-    var faceColor = "brown"
-    var puiplsize1= 40;
-    var puiplsize2= 30;
-    var facesize= 420;
-    function draw(){
-    background(200);
-         
-     //face
+function draw(){
+    background ("gray");
+
+var x= width/2;
+var y= height/2;
+var y = height/2;
+var s = 200; // size
+var o = 50; // offset
+ var eyesize= 50;
+var faceColor = "brown"
+var puiplsize1= 40;
+var puiplsize2= 30;
+var facesize= 420;
+
+//face
     rectMode(CENTER)
     fill(faceColor);
     ellipse (220, 20, 220, facesize);
@@ -41,8 +47,14 @@ function setup(){
     fill("blue")
     rect(175, 205, 90, 10, 10);
          
-     //puipls
-    fill("teal")
-    ellipse(260, 70, puiplsize1);
-    ellipse(110, 70, puiplsize2);
+          //puipls
+         fill("teal")
+         ellipse(260, 70, puiplsize1);
+         ellipse(110, 70, puiplsize2);
+    
+    var mouthSize = map(mouseX, 0, width, 5, 200); // move mouth with mouse x
+	mouthSize = max(0, mouthSize); // prevents negative value error
+
+	rect(x, y + o, s/2, mouthSize, 10); // mouth
 }
+
