29cfaa0bc3a0762fd482329ccab48054dc0ee4cf
Jordan721 <42786923+Jordan721@users.noreply.github.com>
2018-10-02 15:29:12 -0400

    Fixed Website a Little and startig with meme
    10/2/2018

---
+++ Meme_or_Logo_Version_1/sketch.js
@@ -0,0 +1,19 @@
+/*
+ Meme V 1
+ Jordan Alexis
+ 10/2/2018
+ rough draft
+ MMP 210 Week 5
+ */
+
+function setup() {
+    createCanvas(500, 500)
+}
+
+function draw() {
+    background(0);
+    fill("red");
+    textSize(52);
+    textFont("Times New Roman");
+    text("MEME", width / 2, height / 2);
+}
