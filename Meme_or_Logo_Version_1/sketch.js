6ea780295468649986c6f4509cc439c5ea7fc9f4
Jordan Alexis <42786923+Jordan721@users.noreply.github.com>
2018-10-10 11:16:32 -0400

    Added Memes
    10/10/2018

--- Meme_or_Logo_Version_1/sketch.js
+++ Meme_or_Logo_Version_1/sketch.js
@@ -1,5 +1,5 @@
 /*
- Meme V 1
+ Meme V 0
  Jordan Alexis
  10/2/2018
  rough draft
@@ -8,18 +8,19 @@
 
 function setup() {
     createCanvas(500, 500)
+
 }
 
-function draw() {
-    background("yellow");
-    fill("red");
-    var meme = "Memes for days son.";
+    function draw() {
+        background("yellow");
+        fill("red");
+        var meme = "Memes for days son.";
 
-    var s = mouseX;
-    textSize(s);
+        var s = mouseX;
+        textSize(s);
 
-    var leadingSize = mouseY;
-    textLeading(leadingSize);
+        var leadingSize = mouseY;
+        textLeading(leadingSize);
 
-    text(meme, 20, 20, 400, 400);
-}
+        text(meme, 20, 20, 400, 400);
+    }
