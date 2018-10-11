005a8f290da1380d182240bf0f92d078185a31b8
Jordan Alexis <42786923+Jordan721@users.noreply.github.com>
2018-10-11 11:42:07 -0400

    Nothing new
    10/11/2018

--- Meme_or_Logo_Version_1/sketch.js
+++ Meme_or_Logo_Version_1/sketch.js
@@ -6,7 +6,11 @@
  MMP 210 Week 5
  */
 
+var img;
 
+function preload() {
+    img = loadImage("Meme's_pic.png");
+}
 
 function setup() {
     createCanvas(500, 500)
@@ -16,8 +20,11 @@ function draw(){
     background("yellow")
     fill("red");
     var meme = "Memes for days son.";
-
-    var s = mouseX;
+    
+    text(meme, 20, 20, 400, 400);
+    image(img, 20, 20, 400, 400);
+    
+     var s = mouseX;
     textSize(s);
 
     var leadingSize = mouseY;
@@ -25,12 +32,4 @@ function draw(){
     var leadingSize = mouseY;
     textLeading(leadingSize);
     
-    text(meme, 20, 20, 400, 400);
-    image(img, 20, 20, 400, 400);
-    
-}
-var img;
-
-function preload() {
-    img = loadImage("Meme's_pic.png");
 }
