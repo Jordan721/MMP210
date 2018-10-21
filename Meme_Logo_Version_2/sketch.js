fb1ca284ebf756c0b6ba305d66515af9186df616
Jordan Alexis <42786923+Jordan721@users.noreply.github.com>
2018-10-21 13:11:59 -0400

    Finished with MEME Version 1
    10/21/2018

--- Meme_Logo_Version_2/sketch.js
+++ Meme_Logo_Version_2/sketch.js
@@ -7,52 +7,97 @@
  */
 
 var img;
+var img2;
+
 
 function preload() {
-    img = loadImage("Memes_pic.png");
+    img = loadImage("Memes_Pic.png");
+    img2 = loadImage("Memes_2.jpg");
+    img3= loadImage("Memes_3.jpg")
 }
+
 
 function setup() {
-    createCanvas(500, 500)
+    createCanvas(930, 500);
+
 }
 
+
 function draw() {
-    fill("red");
-    var meme = "Memes for days son.";
+    background(0);
 
-    var s = mouseX;
-    textSize(s);
+    // background 4 meme 1
 
-    var leadingSize = mouseY;
-    textLeading(leadingSize);
-    var leadingSize = mouseY;
-    textLeading(leadingSize);
+    var bgX = 130;
+    var bgY = 40;
+    var bgWidth = 320;
+    var bgHeight = 25;
 
-    image(img, 20, 20, 400, 400);
-    //text(meme, 20, 20, 400, 400);
 
-    strokeWeight(1);
-    stroke("white");
-    line(width / 2, 0, width / 2, height);
-    line(0, height / 2, width, height / 2);
-    noStroke();
 
-    if (mouseX < width / 2 && mouseY < height / 2) {
-        // top left
-        fill('gray');
-    } else if (mouseX > width / 2 && mouseY < height / 2) {
-        // top right
-        fill('blue');
-    } else if (mouseX < width / 2 && mouseY > height / 2) {
-        // bottom left
-        fill('black');
+
+    // background 4 meme 2
+
+    var bgX2 = 170;
+    var bgY2 = 390;
+    var bgWidth2 = 257;
+    var bgHeight2 = 25;
+
+
+
+
+    //text
+
+    var textFill = 'blue';
+    var textFill2 = 'orange';
+
+
+    //meme image 
+
+
+    if (mouseX > width / 2) {
+
+        image(img, 0, 0, width / 2, height / 2); // top left
+        image(img2, width / 2, 0, width / 2, height); // right half
+        image(img3, 0, height / 2, width / 2, height / 2); // bottom left
+
+
     } else {
-        // bottom right
-        fill('teal');
+        image(img, 0, 0, width / 2, height / 2); // top left 
+        image(img2, width / 2, height / 2, width / 2, 0); // bottom right 
+
+        textSize(35);
+        text("Memes am I right", 520, 100);
+        text("Ok that's just mean", 100, 400);
     }
-    
+
+
+
+
+
+    fill(textFill);
+
+    textSize(25);
+    textFont("Impact, Charcoal, sans-serif");
+
+
+
+
     if (mouseIsPressed) {
-        var s = (mouseX - pmouseX) + (mouseY - pmouseY);
-        ellipse(mouseX, mouseY, s);
+
+        image(img, 140, 0);
+
+
+        rect(bgX, bgY, bgWidth, bgHeight) // background for words
+
+        rect(bgX2, bgY2, bgWidth2, bgHeight2) // background for words2
+
+
+        fill(textFill2);
+        text("Memes for days ", 132, 63);
+        text("Cry all you want its happening", 172, 413);
+
     }
+
+
 }
