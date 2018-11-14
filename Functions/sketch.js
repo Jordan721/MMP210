8cdbb1994fd2605d92df2b7902b1d130bbe37bd4
Jordan Alexis <42786923+Jordan721@users.noreply.github.com>
2018-11-14 16:22:17 -0500

    11/14/2018
    Trying this with my sketch

--- Functions/sketch.js
+++ Functions/sketch.js
@@ -3,6 +3,7 @@ Week 11
 Jordan Alexis 
 Functions and Interaction
 */
+
 function setup() {
     createCanvas(640, 360);
 }
@@ -16,7 +17,32 @@ var x = 100,
     h = 100;
 
 function draw() {
-    rect(x, y, w, h);
+    //rect(x, y, w, h);
+    
+    //face
+    rectMode(CENTER);
+    fill("brown");
+    ellipse(220, 20, 220, 420);
+
+    //stripe
+    fill("black");
+    rect(220, 20, 60, 340);
+
+
+    //eyes
+    fill("orange");
+    ellipse(250, 90, 70);
+    ellipse(120, 70, 60);
+
+    //nose
+    stroke("gray");
+    fill("gray");
+    rect(175, 125, 50, 50);
+
+    //mouth
+    stroke("red");
+    fill("blue");
+    rect(175, 205, 90, 10, 10);
 }
 
 function mousePressed() {
@@ -27,8 +53,6 @@ function mousePressed() {
         fill(r, g, b);
         ellipse(random(width), random(height), random(20, 100));
 
-        function draw() {
-            background(200);
-        }
+
     }
 }
