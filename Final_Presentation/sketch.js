f27ba885892f565b9e7c20039c7a54a84f8be238
Jordan Alexis <42786923+Jordan721@users.noreply.github.com>
2018-11-20 18:05:45 -0500

    11/20/2018
    almsot done with presentation just have to resize images also add nav bars to all of the site tommorrow

--- Final_Presentation/sketch.js
+++ Final_Presentation/sketch.js
@@ -8,17 +8,21 @@ var slide = 0;
 var adu;
 var poten;
 var breadb;
+var ani;
+var AFrame;
 
 function preload() {
     adu = loadImage('Photos_Final/poten.jpg');
     poten = loadImage('Photos_Final/kit.jpg');
     breadb = loadImage('Photos_Final/breadb.jpg');
+    AFrame = loadImage('Photos_Final/AFrame_Pic.png')
+    ani = loadImage('Photos_Final/Sony_Pictures_Animation_logo.png')
 }
 
 function setup() {
     createCanvas(800, 500);
     textSize(60);
-    textFont('monospace');
+    textFont('Allerta Stencil');
     textAlign(CENTER, CENTER);
     rectMode(CENTER);
 }
@@ -29,17 +33,21 @@ function draw() {
         text("Final Project Idea Animated", width / 2, height / 2);
     } else if (slide == 1) {
         textSize(30);
-        text("Force resisiting sensor", 100, 100);
+        text("Sensor I'm using to animate A-frame", 100, 100);
+        // potentiometer pic
+        image(poten, 400, 100, 40, 60);
+        image(adu, 200, 100, 40, 60);
+        image(breadb, 500, 100, 40, 60);
 
-        // fsr graphic
-        rect(width / 2, 300, 50, 300);
-        ellipse(width / 2, 200, 150);
     } else if (slide == 2) {
         text("theme: animation", 200, 20);
-        image(adu, 200, 100, 40, 60);
-        image(poten, 400, 100, 40, 60);
-        image(breadb, 500, 100, 40, 60);
+        image(ani, 200, 100, 40, 60);
+
+    } else if (slide == 3) {
+        text("Image of Aframe", 200, 20);
+        image(AFrame, 200, 100, 40, 60);
     }
+
 
 }
 
