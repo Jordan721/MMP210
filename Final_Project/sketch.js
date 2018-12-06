3ecf31fde40d674d19d128a4204c0f4a2db68f03
Jordan Alexis <42786923+Jordan721@users.noreply.github.com>
2018-12-06 16:57:29 -0500

    12/6/2018
    Made addjustemnts to Final Project

--- Final_Project/sketch.js
+++ Final_Project/sketch.js
@@ -2,6 +2,13 @@
 Final Project 
 Jordan Alexis 
 */
+var NewAge;
+var bing;
+
+function preload() {
+    NewAge = loadSound('new-age-pad.wav');
+    bing = loadSound('new-age-brass.wav')
+}
 
 function setup() {
     createCanvas(900, 500, WEBGL);
@@ -9,9 +16,20 @@ function setup() {
 }
 
 function draw() {
-    background("black");
+    if (NewAge.isPlaying) {
+        background("black");
+    }
     var radius = width * 1.5;
+    var vol = map(mouseY, 0, height, 1, 0);
+    vol = constrain(vol, 0, 1);
+    NewAge.setVolume(vol);
 
+    var pan = map(mouseX, 0, width, -1, 1);
+    pan = constrain(pan, -1, 1);
+    NewAge.pan(pan);
+
+    var rate = map(mouseX, 0, width, 0.25, 2);
+    rate = constrain(rate, 0.25, 2);
     //drag to move the world.
     orbitControl();
 
@@ -25,12 +43,28 @@ function draw() {
             translate(sin(2 * a) * radius * sin(b), cos(b) * radius / 2, cos(2 * a) * radius * sin(b));
             if (j % 2 === 0) {
                 fill('white')
+                rotateZ(frameCount * 0.02);
+                rotateX(frameCount * 0.02);
                 cone(30, 30);
             } else {
                 fill('blue')
+                rotateZ(frameCount * 0.05);
+                rotateX(frameCount * 0.05);
                 box(30, 30, 30);
             }
             pop();
+        }
+    }
+}
+
+function mousePressed() {
+    if (mouseX > 0 && mouseX < width &&
+        mouseY > 0 && mouseY < height) {
+        if (NewAge.isPlaying()) {
+            NewAge.stop();
+            bing.play();
+        } else {
+            NewAge.play();
         }
     }
 }
